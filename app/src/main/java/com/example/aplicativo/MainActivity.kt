f66e232d0279e8d373673616d24974413cc40c39
rav <ravtec>
2021-01-13 17:42:23 -0300

    commit final

--- app/src/main/java/com/example/aplicativo/MainActivity.kt
+++ app/src/main/java/com/example/aplicativo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.aplicativo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     val res = arrayOf("Sim", "Não", "Talvez")
     val random = Random.Default
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(0)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
